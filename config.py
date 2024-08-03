9d8eaeb956f23f734bd810438fd13e81b0e900d7
TEDZOBOT <94851911+Tedzo555@users.noreply.github.com>
2024-08-03 19:30:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
-FORCE_SUB   = ("hy0y0", "")
+import re
+from os import environ
+
+id_pattern = re.compile(r'^.\d+$')
+
+AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('-1002190822875', '').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
+
