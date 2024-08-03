16da0d30c5d953fba25229dcf028bdeb2704c5da
TEDZOBOT <94851911+Tedzo555@users.noreply.github.com>
2024-08-03 16:42:57 +0530

    Delete plugins/test.py

--- plugins/test.py
+++
@@ -1,8 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-import asyncio
-
-
-@Client.on_message(filters.group & filters.command("start")) 
-async def id_message(bot, msg):
-    await msg.send_reaction(chat_id, message_id=message_id, emoji="🔥")
