13f3ff81c9b4e5656249d3425b6ae51e6ea61740
TEDZOBOT <94851911+Tedzo555@users.noreply.github.com>
2024-08-03 19:21:23 +0530

    Update Force_sub.py

--- plugins/Force_sub.py
+++ plugins/Force_sub.py
@@ -1,30 +1,46 @@
-from pyrogram import Client, filters, enums
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from pyrogram.errors import UserNotParticipant
-from config import FORCE_SUB
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
 
-async def not_subscribed(_, client, message):
-    # If FORCE_SUB is not defined, skip the subscription check
-    if not Config.FORCE_SUB:
-        return False
-    try:
-        user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id)
-        if user.status == enums.ChatMemberStatus.BANNED:
-            return True
-        else:
-            return False
-    except UserNotParticipant:
-        pass
-    return True
 
-@Client.on_message(filters.private & filters.create(not_subscribed))
-async def forces_sub(client, message):
-    buttons = [[InlineKeyboardButton(text=" 🔥 Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ 🔥 ", url=f"https://t.me/{Config.FORCE_SUB}")]]
-    text = "**Sᴏʀʀy Dᴜᴅᴇ, Yᴏᴜ Hᴀᴠᴇ Nᴏᴛ Jᴏɪɴᴇᴅ My Uᴩᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ 😐 \n Pʟᴇᴀꜱᴇ Jᴏɪɴ My Uᴩᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ Tᴏ Usᴇ Mᴇ **"
-    try:
-        user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id)
-        if user.status == enums.ChatMemberStatus.BANNED:
-            return await client.send_message(message.from_user.id, text="Sᴏʀʀy Yᴏᴜ'ʀᴇ Bᴀɴɴᴇᴅ Tᴏ Uꜱᴇ Mᴇ")
-    except UserNotParticipant:
-        return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
-    return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
+# info.py / config.py / vars.py file..
+import re
+from os import environ
+
+id_pattern = re.compile(r'^.\d+$')
+
+AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('-1002190822875', '').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
+
+
+# force subscribe main code :-
+from info import AUTH_CHANNEL
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import *
+
+async def is_subscribed(bot, query, channel):
+    btn = []
+    for id in channel:
+        chat = await bot.get_chat(int(id))
+        try:
+            await bot.get_chat_member(id, query.from_user.id)
+        except UserNotParticipant:
+            btn.append([InlineKeyboardButton(f'Join {chat.title}', url=chat.invite_link)])
+        except Exception as e:
+            pass
+    return btn
+
+#@Client.on_message.....
+#async def start(....
+    if AUTH_CHANNEL:
+        try:
+            btn = await is_subscribed(client, message, AUTH_CHANNEL)
+            if btn:
+                username = (await client.get_me()).username
+                if message.command[1]:
+                    btn.append([InlineKeyboardButton("♻️ Try Again ♻️", url=f"https://t.me/{username}?start={message.command[1]}")])
+                else:
+                    btn.append([InlineKeyboardButton("♻️ Try Again ♻️", url=f"https://t.me/{username}?start=true")])
+                await message.reply_text(text=f"<b>👋 Hello {message.from_user.mention},\n\nPlease join the channel then click on try again button. 😇</b>", reply_markup=InlineKeyboardMarkup(btn))
+                return
+        except Exception as e:
+            print(e)
