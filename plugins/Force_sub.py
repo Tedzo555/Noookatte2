dc16fd895eaf3db4b75536cbee2569be005cc1f2
TEDZOBOT <94851911+tedzooo@users.noreply.github.com>
2024-08-10 11:36:32 +0530

    sub.py

--- plugins/Force_sub.py
+++ plugins/Force_sub.py
@@ -9,20 +9,20 @@ I am a usless Bot**
 >> `I can generate text to QR Code with QR Code decode to text support.`"""
 
 buttons = [[
-                InlineKeyboardButton('↖️ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘs ↗️', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+                InlineKeyboardButton('↖️ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘs ↗️', url=f'http://t.me/{username}?startgroup=true')
                 ],[
                 InlineKeyboardButton('🧞‍♀️ Sᴇᴀʀᴄʜ', switch_inline_query_current_chat=''),
-                InlineKeyboardButton('🔍 Gʀᴏᴜᴘ', url=f'https://t.me/{MOVIE_GROUP_USERNAME}')
+                InlineKeyboardButton('🔍 Gʀᴏᴜᴘ', url=f'https://t.me/tedzomovie01')
                 ],[
                 InlineKeyboardButton('🙆🏻 Hᴇʟᴘ ', callback_data='help'),
-                InlineKeyboardButton('🎁 Hᴇʟᴘ++', callback_data='leech_url_help'),
+                InlineKeyboardButton('🎁 Hᴇʟᴘ++', callback_data='home'),
                 ],[
-                InlineKeyboardButton('⚙ Sᴇᴛᴛɪɴɢs', callback_data='openSettings'),
+                InlineKeyboardButton('⚙ Sᴇᴛᴛɪɴɢs', callback_data='close'),
                 InlineKeyboardButton('♥️ Aʙᴏᴜᴛ', callback_data='about')
                 ],[
-                InlineKeyboardButton('⪦ 𝕄𝕆𝕍𝕀𝔼 ℂℍ𝔸ℕℕ𝔼𝕃 ⪧', url='https://t.me/real_MoviesAdda3')
+                InlineKeyboardButton('⪦ BOT ⪧', url='https://t.me/tedzo_bot')
                 ],[
-                InlineKeyboardButton('🎉 Learn BOT making 🎊', url="https://youtube.com/@LazyDeveloperr")
+                InlineKeyboardButton('🎉 Learn BOT making 🎊', url="https://youtube.com/@mrbeast")
                 ]]
 
 async def is_subscribed(bot, query, channel):
@@ -59,3 +59,30 @@ async def start_bot(client, message):
       	reply_markup=buttons,
       	quote=True
                 )
+
+@Client.on_callback_query()
+async def cb_data(bot, update):
+    
+    if update.data == "home":
+        await update.message.edit_text(
+            text=START_TEXT.format(update.from_user.mention),
+            disable_web_page_preview=True,
+            reply_markup=buttons
+        )
+    elif update.data == "close":
+        await update.answer("Closed")
+        await update.message.delete()
+
+    elif update.data == "help":
+        await update.message.edit_text(
+            text=START_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=buttons
+        )
+    
+    elif update.data == "about":
+        await update.message.edit_text(
+            text=START_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=buttons
+        )
