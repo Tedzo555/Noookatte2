a1ba38c676bb017c052282d322b22b82d64e93bb
TEDZOBOT <94851911+tedzooo@users.noreply.github.com>
2024-08-10 09:46:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from os import environ
 
 id_pattern = re.compile(r'^.\d+$')
 
-AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('-1002190822875', '').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
+AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('-1002190822875', '-1002190822875').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
 
